80c4b333f1e2fb28a0946749372bea86780ce086
alimardon <aboqijonov12@gmail.com>
2023-12-15 21:04:00 +0500

    contact, detail, filter, search

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email')
+    list_display_links = ('id', 'name', 'email')
+    readonly_fields = ['pub_date']
+
+
+admin.site.register(Contact, ContactAdmin)
